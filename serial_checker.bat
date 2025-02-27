9cac4ed615fc1d58266ff2f6eb29513e4731e74c
sa34t ( metix ) <LOWSTUDIOSCONTACT@GMAIL.COM>
2025-02-27 20:40:31 +0100

    Add files via upload

---
+++ serial_checker.bat
@@ -0,0 +1,36 @@
+
+@echo off
+title Serial Checker made free by metix
+echo.
+color 90
+echo Serial Checker
+echo.
+color 90 
+:start
+color 4  
+cls                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+echo [91m       Disk Number
+echo [90m==========================[97m
+wmic diskdrive get serialnumber
+echo [93m      Motherboard
+echo [90m==========================[97m
+wmic baseboard get serialnumber
+echo [92m        SMBios
+echo [90m==========================[97m
+wmic path win32_computersystemproduct get uuid
+echo [96m         GPU
+echo [90m==========================[97m
+wmic PATH Win32_VideoController GET Description,PNPDeviceID
+echo [94m         RAM
+echo [90m==========================[97m
+wmic memorychip get serialnumber
+echo [95m         Bios
+echo [90m==========================[97m
+wmic csproduct get uuid
+echo [91m         CPU
+echo [90m==========================[97m
+wmic cpu get processorid
+echo Press any key to get your hardware serials again.
+echo by metix.
+pause>nul
+goto start
